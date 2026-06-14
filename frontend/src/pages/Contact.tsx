c4b75019ce07b3b06d085126c8396f22448496df
Emmy Manning <emswoo@EM.local>
2026-06-14 14:19:54 -0400

    refactor: rename api endpoint

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -21,7 +21,7 @@ const Contact = () => {
     // console.log("data:", data);
 
     try {
-      const res = await fetch(`/api/emails`, {
+      const res = await fetch(`/api/email`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
