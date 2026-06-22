b1e2a6a4047d18bd222a8e18854c74a60c0a70f8
Emmy Manning <emswoo@EM.local>
2026-06-22 12:38:01 -0400

    feature: add send email function and test

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -1,7 +1,58 @@
 import json
+import os
+import boto3
+
+ses = boto3.client("ses")
 
 def lambda_handler(event, context):
+    print("Lambda: START")
+    print("Lambda event", event)
+
+    try:
+        body = json.loads(event.get("body") or "{}")
+
+        # validation
+        name = body.get("name", "").strip()
+        email = body.get("email", "").strip()
+        message = body.get("message", "").strip()
+
+        if not all([name, email, message]):
+            return response(400, {
+                "ok": False,
+                "error": "name, email, message are required."
+            })
+
+        print("SES: START")
+
+        # ses
+        ses.send_email(
+            Source=os.environ["FROM_EMAIL"],
+            Destination={"ToAddresses": [os.environ["TO_EMAIL"]]},
+            Message={
+                "Subject": {"Data": f"Contact from {name}."},
+                "Body": {
+                    "Text": {
+                        "Data": f"Name: {name}\nEmail: {email}\n\nMessage:\n{message}\n"
+                    }
+                },
+            },
+            ReplyToAddresses=[email],
+        )
+
+        return response(200, {"ok": True})
+
+    except Exception as err:
+        print("SES send error:", err)
+        return response(500, {
+            "ok": False,
+            "error": "Failed to send email..."
+        })
+
+
+def response(status_code, body):
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        "statusCode": status_code,
+        "body": json.dumps(body)
     }
+
+print("SES: SENT")
