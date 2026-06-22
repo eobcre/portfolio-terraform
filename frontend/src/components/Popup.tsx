8f1e17354cc8ecc3dd99287dc4de8b4cad0fa070
Emmy Manning <emswoo@EM.local>
2026-06-22 12:24:23 -0400

    style: fix popup width

--- frontend/src/components/Popup.tsx
+++ frontend/src/components/Popup.tsx
@@ -7,7 +7,7 @@ type PopupType = {
 const Popup = ({ popupTitle, popupMessage, onClick }: PopupType) => {
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-black/40">
-      <div className="bg-white shadow-lg text-center rounded-xl p-6 w-90">
+      <div className="bg-white shadow-lg text-center rounded-xl p-6 w-125">
         <h2 className="text-gray-900 text-lg font-semibold">{popupTitle}</h2>
         <p className="text-gray-600 mt-2">{popupMessage}</p>
         <button onClick={onClick} className="bg-black text-white rounded-lg mt-4 px-4 py-2">
